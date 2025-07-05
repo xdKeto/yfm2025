1b89f4d7575e96d0994a0c315fbb6ed544a50563
Yestoya L Minggus <yestoya.minggus@gmail.com>
2025-07-06 01:24:34 +0700

    tagline size

--- yfm-2025/src/components/Hero.jsx
+++ yfm-2025/src/components/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
       {/* Logo and tagline group */}
       <div className="flex flex-col items-center justify-center space-y-4 md:mt-16 relative z-10">
         <img src={yfmLogo} width={580} height={650} alt="" />
-        <img src={tagline} width={350} height={650} alt="" />
+        <img src={tagline} width={300} height={650} alt="" />
       </div>
       <div class="custom-shape-divider-bottom-1751734920">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
