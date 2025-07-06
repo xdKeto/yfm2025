c3a74ae57342f542249ab633cbe18b4d941d667a
Yestoya L Minggus <yestoya.minggus@gmail.com>
2025-07-06 11:05:14 +0700

    added analytics

--- yfm-2025/src/App.jsx
+++ yfm-2025/src/App.jsx
@@ -5,10 +5,12 @@ import About from "./components/About";
 import Mancing from "./components/Mancing";
 import Makrab from "./components/Makrab";
 import Contacts from "./components/Contacts"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
+    <Analytics/>
     <Navbar/>
       <main className="antialiased overflow-x-hidden overflow-y-hidden mx-auto relative z-10">
     <Navbar/>
